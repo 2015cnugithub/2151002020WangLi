96d2fc1ab373ed553422d9a4833876a93b360809
wangli <wang-city@163.com>
2015-10-19 19:35:23 +0800

    delete for recommit

--- OOH_3_20151018/Debug/src/SalariedWorker.d
+++
@@ -1,6 +0,0 @@
-src/SalariedWorker.o: ../src/SalariedWorker.cpp ../src/SalariedWorker.h \
- ../src/Worker.h
-
-../src/SalariedWorker.h:
-
-../src/Worker.h:
--- OOH_3_20151018/Debug/src/Test.d
+++
@@ -1,8 +0,0 @@
-src/Test.o: ../src/Test.cpp ../src/Worker.h ../src/HourlyWorker.h \
- ../src/SalariedWorker.h
-
-../src/Worker.h:
-
-../src/HourlyWorker.h:
-
-../src/SalariedWorker.h:
